0e8105aa985e2d795475d0ba8b5270ecffde0f0a
Gizem meral <gizemmeraldesign@gmail.com>
2025-04-13 18:37:52 +0300

    Functions

---
+++ 0-crash-course/6-Functions.py
@@ -0,0 +1,50 @@
+# def greet_everyone():
+#     print("Hello, everyone!")
+
+# greet_everyone()
+
+
+# def greet(name):
+#     print(f"Hello, {name}!")
+
+# greet("Alice")
+# greet("Bob")
+# greet("Charlie")
+
+# user1 = "Alice"
+# print(f"Hello, {user1}!","Welcome to our app")
+# print("We hope you enjoy your stay!")
+# print("Please let us know if you have any questions.")
+
+
+# user2 = "John"
+# print(f"Hello, {user2}!","Welcome to our app")
+# print("We hope you enjoy your stay!")
+# print("Please let us know if you have any questions.")
+
+# def greeet_user(name):
+#     print(f"Hello, {name}!","Welcome to our app")
+#     print("We hope you enjoy your stay!")
+#     print("Please let us know if you have any questions.")
+
+
+# greeet_user("Alice")
+# greeet_user("John")
+# greeet_user("Bob")
+# greeet_user("Charlie")
+# greeet_user("David")
+# greeet_user("Eve")
+# greeet_user("Frank")
+# greeet_user("Grace")
+# greeet_user("Hannah")
+
+
+def power(base,exponent):
+    return base ** exponent
+
+x = power(2,3) #8
+y = power(3,2) #9
+z = power(5,2) #25
+
+print(x,y,z)
+print(power(3,2),power(5,2),power(2,3)) #9 25 8

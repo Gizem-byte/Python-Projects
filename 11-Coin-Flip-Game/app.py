21396588639decac8a012030d081b7a940d5918f
Gizem meral <gizemmeraldesign@gmail.com>
2025-04-14 12:13:02 +0300

    project 11

---
+++ 11-Coin-Flip-Game/app.py
@@ -0,0 +1,34 @@
+import random
+
+print("COIN FLIP GAME")
+print("Guess heads or tails!")
+
+
+while True:
+
+   user_input = input("Enter your guess (heads/tails):")
+
+   coin = ["heads","tails"]
+   result = random.choice(coin)
+
+   print(f"The coin shows: {result}")
+
+   if user_input.lower() == result :
+      print("You guessed correctly! you win!")
+      user_answer = input ("Play again ? (y/n)")
+
+      if user_answer.lower() == "y":
+         continue
+      else:
+         print("Thank you for playing")
+         break
+
+   else:
+      print("Sorry wrong guess, Try again")
+      user_answer = input ("Play again ? (y/n)")
+
+      if user_answer.lower() == "y":
+         continue
+      else:
+         print("Thank you for playing")
+         break
